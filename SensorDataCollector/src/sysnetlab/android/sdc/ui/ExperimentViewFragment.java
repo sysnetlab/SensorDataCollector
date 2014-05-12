09fbefa90ca298c4dae3415b09c088bdb553b02e
graychan <gray.chenhui@gmail.com>
2014-05-12 18:34:02 -0400

    added classes to view an experiment

---
+++ SensorDataCollector/src/sysnetlab/android/sdc/ui/ExperimentViewFragment.java
@@ -0,0 +1,72 @@
+package sysnetlab.android.sdc.ui;
+
+import sysnetlab.android.sdc.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class ExperimentViewFragment extends Fragment {
+	private View mView;
+	private ExperimentTagsFragment mExperimentTagsFragment;
+	private ExperimentSensorSelectionFragment mExperimentSensorSelectionFragment;
+	private ExperimentViewFragmentHandler mHandler;
+	
+	public interface ExperimentViewFragmentHandler {
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+
+		mView = inflater.inflate(R.layout.fragment_experiment_view, container,
+				false);
+		
+		// use nested fragment
+		FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+
+
+		if (mExperimentTagsFragment == null) {
+			mExperimentTagsFragment = new ExperimentTagsFragment();;
+			transaction.add(R.id.layout_experiment_view_tags, mExperimentTagsFragment);
+		}
+		
+		if (mExperimentSensorSelectionFragment == null) {
+			mExperimentSensorSelectionFragment = new ExperimentSensorSelectionFragment();
+			transaction.add(R.id.layout_experiment_view_sensor_list,  mExperimentSensorSelectionFragment);
+		}
+		transaction.commit();		
+		
+		return mView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		((EditText)mView.findViewById(R.id.et_experiment_view_name))
+		.setText(((ViewExperimentActivity)getActivity()).getExperiment().getName());
+
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		
+		try {
+			mHandler = (ExperimentViewFragmentHandler)activity;
+		} catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement ExperimentViewFragment.ExperimentViewFragmentHandler");
+        }
+	}
+
+	
+
+}
---
+++ SensorDataCollector/src/sysnetlab/android/sdc/ui/ViewExperimentActivity.java
@@ -0,0 +1,61 @@
+package sysnetlab.android.sdc.ui;
+
+import sysnetlab.android.sdc.R;
+import sysnetlab.android.sdc.datacollector.Experiment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+
+public class ViewExperimentActivity extends FragmentActivity implements
+		ExperimentViewFragment.ExperimentViewFragmentHandler,
+		ExperimentTagsFragment.OnFragmentEventListener {
+	private ExperimentViewFragment mExperimentViewFragment;
+	private Experiment mExperiment;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO handle configuration change
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_container);
+		
+		Intent intent = getIntent();
+		mExperiment = intent.getParcelableExtra("experiment");
+		if (mExperiment == null)
+			Log.i("SensorDataColelctor", "ViewExperimentActivity failed to get experiment from intent");
+		Log.i("SensorDataCollector", "ViewExperimentActivity: experiment is " + mExperiment.toString());
+
+		if (findViewById(R.id.fragment_container) != null) {
+			if (savedInstanceState != null) {
+				return;
+			}
+
+			mExperimentViewFragment = new ExperimentViewFragment();
+			FragmentTransaction transaction = getSupportFragmentManager()
+					.beginTransaction();
+			transaction.add(R.id.fragment_container, mExperimentViewFragment);
+			transaction.commit();
+		}
+
+		Log.i("SensorDataCollector", "ViewExperimentActivity.onCreate called.");
+	}
+	
+	public Experiment getExperiment() {
+		return mExperiment;
+	}
+
+
+
+	@Override
+	public void onTxtFldEnterPressed_ExperimentTagsFragment() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onBtnLabelClicked_ExperimentTagsFragment() {
+		// TODO Auto-generated method stub
+		
+	}
+}
