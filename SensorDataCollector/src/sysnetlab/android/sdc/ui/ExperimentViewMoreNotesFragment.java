842174306a20a2c35ad748698acccf82bc7e4e15
graychan <gray.chenhui@gmail.com>
2014-05-18 01:18:11 -0400

    made progress in viewing notes in experiment view (incomplete)

---
+++ SensorDataCollector/src/sysnetlab/android/sdc/ui/ExperimentViewMoreNotesFragment.java
@@ -0,0 +1,7 @@
+package sysnetlab.android.sdc.ui;
+
+import android.support.v4.app.Fragment;
+
+public class ExperimentViewMoreNotesFragment extends Fragment {
+
+}
--- SensorDataCollector/src/sysnetlab/android/sdc/ui/ExperimentViewNotesFragment.java
+++ SensorDataCollector/src/sysnetlab/android/sdc/ui/ExperimentViewNotesFragment.java
@@ -6,15 +6,49 @@ import java.util.ArrayList;
 import sysnetlab.android.sdc.R;
 import sysnetlab.android.sdc.datacollector.ExperimentManagerSingleton;
 import sysnetlab.android.sdc.datacollector.Note;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ExperimentViewNotesFragment extends Fragment {
     private View mView;
+    private OnFragmentClickListener mCallback;    
+    
+    public interface OnFragmentClickListener {
+        public void onImageViewMoreOrLessNotesClicked_ExperimentViewNotesFragment();
+    }    
+    
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        
+        try {
+            mCallback = (OnFragmentClickListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentClickListener");
+        }        
+    }
+    
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        
+        ((ImageView) mView.findViewById(R.id.imageview_notes_plusminus))
+        .setOnClickListener(new ImageView.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                mCallback.onImageViewMoreOrLessNotesClicked_ExperimentViewNotesFragment();
+
+            }
+        });        
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,13 +59,14 @@ public class ExperimentViewNotesFragment extends Fragment {
 
         ArrayList<Note> lstNotes = ExperimentManagerSingleton.getInstance().getActiveExperiment()
                 .getNotes();
+        
         if (lstNotes != null && !lstNotes.isEmpty()) {
             Note note = lstNotes.get(0);
             ((TextView) mView.findViewById(R.id.textview_fragment_note_viewing_note))
                     .setText(note.getNote());
         } else {
             ((TextView) mView.findViewById(R.id.textview_fragment_note_viewing_note))
-                    .setText("No notes found.");
+                    .setText(getResources().getString(R.string.text_epxeriment_has_no_notes));
         }
 
         return mView;
