eecc0edabc9be7b2d6e287d9ffaa762df0a9310d
dcwalter <dwalter@gmail.com>
2014-06-17 15:17:17 -0400

    Improvements to Dropbox Feature
    - If a Dropbox account is not yet linked when the user tries to send an
    experiment to Dropbox, he/she will be given the opportunity to link
    their account immediately.
    - While files are being uploaded to Dropbox, an ActivityCircle is
    displayed with an appropriate message.

--- SensorDataCollector/src/sysnetlab/android/sdc/ui/ViewExperimentActivity.java
+++ SensorDataCollector/src/sysnetlab/android/sdc/ui/ViewExperimentActivity.java
@@ -11,6 +11,8 @@ import sysnetlab.android.sdc.ui.fragments.ExperimentViewFragment;
 import sysnetlab.android.sdc.ui.fragments.ExperimentViewNotesFragment;
 import sysnetlab.android.sdc.ui.fragments.ExperimentViewSensorDataFragment;
 import sysnetlab.android.sdc.ui.fragments.FragmentUtil;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -63,6 +65,19 @@ public class ViewExperimentActivity extends FragmentActivity implements
         Log.i("SensorDataCollector", "ViewExperimentActivity.onCreate called.");
     }
     
+    public void onResume() {
+    	super.onResume();
+    	
+        // Complete the Dropbox Authorization
+        DropboxHelper.getInstance().completeAuthentication();
+        // The attempt to link to Dropbox must have occurred as a result of
+        // an attempt to upload to Dropbox.  If the link was successful,
+        // go ahead and complete the upload process.  
+//        if (DropboxHelper.getInstance().isLinked()) {
+//        	DropboxHelper.getInstance().writeAllFilesInDirToDropbox(mExperiment.getPath());
+//        }
+    }
+    
     @Override
     public void onBtnCloneClicked_ExperimentViewFragment() {
         Intent intent = new Intent(this, CreateExperimentActivity.class);
@@ -74,7 +89,30 @@ public class ViewExperimentActivity extends FragmentActivity implements
     @Override 
     public void onBtnDropboxClicked_ExperimentViewFragment() {
     	DropboxHelper dbHelper = DropboxHelper.getInstance();
-    	dbHelper.writeAllFilesInDirToDropbox(mExperiment.getPath());
+    	if (!dbHelper.isLinked()) {
+    		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    		
+            builder.setMessage(R.string.text_dropbox_not_yet_linked_explanation);
+            builder.setTitle(R.string.text_link_to_dropbox);
+            builder.setPositiveButton(R.string.text_link_to_dropbox,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                        	dialog.dismiss();
+                            DropboxHelper.getInstance().link();                      
+                        }
+                    });
+            builder.setNegativeButton(R.string.text_do_not_link_to_dropbox,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                        }
+                    });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+    	}
+    	else {
+    		dbHelper.writeAllFilesInDirToDropbox(mExperiment.getPath(), ViewExperimentActivity.this);
+    	}
     }
 
     @Override
