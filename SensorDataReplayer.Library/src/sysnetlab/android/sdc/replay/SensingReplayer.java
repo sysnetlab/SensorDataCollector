01cb514675c9132fe0f45f679c10664544fcad36
Kosta Damevski <kostadin@gmail.com>
2014-12-11 10:50:07 -0500

    sync up

--- SensorDataReplayer.Library/src/sysnetlab/android/sdc/replay/SensingReplayer.java
+++ SensorDataReplayer.Library/src/sysnetlab/android/sdc/replay/SensingReplayer.java
@@ -3,16 +3,20 @@ package sysnetlab.android.sdc.replay;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 
 public class SensingReplayer {
 
 	private InputStreamReader isReader;
+	private InputStreamReader isMetaDataReader;
 	private SensorEventListener listener;
 	
-	public SensingReplayer(InputStreamReader isReader, SensorEventListener listener)
+	public SensingReplayer(InputStreamReader isReader, InputStreamReader isMetaDataReader, SensorEventListener listener)
 	{
 		this.isReader = isReader;
+		this.isMetaDataReader = isMetaDataReader;
 		this.listener = listener;
 	}
 	
@@ -21,11 +25,38 @@ public class SensingReplayer {
 			BufferedReader br = new BufferedReader(isReader); 		
 			for(String line; (line = br.readLine()) != null; ) 
 			{
-					// process the line.
+				//parse line				
+				//TODO: works for accelerometer only for now
+				String [] strValues = line.split("[\\s,]+");
 				
-					// create a SensorEvent object
+				if(strValues.length > 4)
+				{
+					long timestamp = Long.parseLong(strValues[0]);
 				
-					//listener.onSensorChanged(event);
+					float [] values = new float [3];
+					for(int i=0; i<3; i++)
+					{
+						values[i] = Float.parseFloat(strValues[i+2]);
+					}
+				
+					//create a SensorEvent object
+					//TODO: complete accuracy and sensor, check timestamp
+					
+					/*
+					SensorEvent se = WhiteBox.invokeConstructor(SensorEvent.class, 3);
+					se.timestamp = timestamp;
+					se.values[0] = values[0];
+					se.values[1] = values[1];
+					se.values[2] = values[2];
+					*/
+					
+					//se.values = values;
+					//when(se.values).thenReturn(values);
+					//when(se.accuracy).thenReturn(0);
+					//when(se.sensor).thenReturn(Sensor.TYPE_ACCELEROMETER);
+				
+					//listener.onSensorChanged(se);
+				}
 			}
 			br.close();
 		}
